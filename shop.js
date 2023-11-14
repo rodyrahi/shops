6ff8b4892102fbf153e76e705af3db99afc47a28
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-11-14 11:29:27 +0530

    okdone

--- shop.js
+++ shop.js
@@ -24,9 +24,14 @@ app.get('/getcustomer/:id', function(req, res) {
     const customer = shopdb.prepare(`SELECT * FROM customer WHERE id=?`).all(id)
 
     const products = shopdb.prepare(`SELECT * FROM products`).all()
+
+    const sales = shopdb.prepare(`SELECT * FROM sales WHERE customerid = ?`).all(id)
+
+
+
     console.log(products);
 
-    res.render('partials/customer' , {customer:customer[0] , products});
+    res.render('partials/customer' , {customer:customer[0] , products ,sales});
 });
 
 
@@ -40,17 +45,17 @@ app.post('/specs', function(req, res) {
 });
 app.post('/productssave', function(req, res) {
 
-    const{code , desc ,  qty , rate , amount , dis , discount } = req.body
+    const{code , desc ,  qty , rate , amount , dis , discount ,id} = req.body
 
-    
+    shopdb.prepare(`DELETE FROM sales WHERE customerid = ?`).run(id)
     if (Array.isArray(code)) {
         code.forEach((element ,index) => {
-            shopdb.prepare(`INSERT INTO sales (code , desc , qty ,rate ,amount , dis , discount) VALUES (? , ? ,?,?,?,?,? )`)
-            .run(code[index] , desc[index] , qty[index] , rate[index] , amount[index] , dis[index] , discount[index])
+            shopdb.prepare(`INSERT INTO sales (code , desc , qty ,rate ,amount , dis , discount , customerid) VALUES (? , ? ,?,?,?,?,?,? )`)
+            .run(code[index] , desc[index] , qty[index] , rate[index] , amount[index] , dis[index] , discount[index] , id)
         });
     }else{
-        shopdb.prepare(`INSERT INTO sales (code , desc , qty ,rate ,amount , dis , discount) VALUES (? , ? ,?,?,?,?,? )`)
-        .run(code , desc , qty , rate , amount , dis , discount)
+        shopdb.prepare(`INSERT INTO sales (code , desc , qty ,rate ,amount , dis , discount, customerid) VALUES (? , ? ,?,?,?,?,?,? )`)
+        .run(code , desc , qty , rate , amount , dis , discount , id)
     }
 
 
